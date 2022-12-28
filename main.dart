72ae0f410e8dea85b3f1af00fe9c1ed7a80d8e02
wonjangcloud9 <48057918+wonjangcloud9@users.noreply.github.com>
2022-12-28 20:05:51 +0900

    maps

--- main.dart
+++ main.dart
@@ -1,12 +1,8 @@
 void main() {
-  var oldFriends = ['John', 'Paul', 'George', 'Ringo'];
-  var newFriends = [
-    'John',
-    'Paul',
-    'George',
-    'Ringo',
-    'Yoko',
-    for (var friend in oldFriends) "Hello $friend",
-  ];
-  print(newFriends);
+  Map<int, bool> player = {
+    1: true,
+    2: false,
+    3: false,
+    4: false,
+  };
 }
