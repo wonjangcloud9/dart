2e746e73cfbbdd1c7bfeeff494691068bc999892
wonjangcloud9 <48057918+wonjangcloud9@users.noreply.github.com>
2022-12-28 19:59:43 +0900

    string interpolation

--- main.dart
+++ main.dart
@@ -1,11 +1,6 @@
 void main() {
-  var giveMeFive = true;
-  var numbers = [
-    1,
-    2,
-    3,
-    4,
-    if (giveMeFive) 5,
-  ];
-  print(numbers);
+  var name = "wonjang";
+  var age = 10;
+  var greeting = "Hello, $name and I'm ${age + 18} years old";
+  print(greeting);
 }
