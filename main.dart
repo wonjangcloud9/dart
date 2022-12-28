cdc065cb30074b7aee06ea171d0847eb38f7e0cd
wonjangcloud9 <48057918+wonjangcloud9@users.noreply.github.com>
2022-12-28 19:53:39 +0900

    basic data types

--- main.dart
+++ main.dart
@@ -1,5 +1,10 @@
 void main() {
-  late final String name;
-  name = 'John';
+  String name = "wonjang";
+  bool isTrue = true;
+  int age = 30;
+  double height = 180.5;
   print(name);
+  print(isTrue);
+  print(age);
+  print(height);
 }
