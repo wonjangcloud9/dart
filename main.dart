139cd675d668421d9f1d1e0bbeb9c15e099ae84f
wonjangcloud9 <48057918+wonjangcloud9@users.noreply.github.com>
2022-12-28 20:53:30 +0900

    named parameters

--- main.dart
+++ main.dart
@@ -1,5 +1,15 @@
-String sayHello(String name) => 'Hello $name';
+String sayHello({
+  required String name,
+  required int age,
+  required String country,
+}) {
+  return "Hello $name, you are $age years old and you are from $country";
+}
 
 void main() {
-  print(sayHello('John'));
+  print(sayHello(
+    name: "John",
+    age: 20,
+    country: "USA",
+  ));
 }
