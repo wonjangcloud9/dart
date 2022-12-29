8a298fbb58aa54260818e6c3c1173c946db4f370
wonjangcloud9 <48057918+wonjangcloud9@users.noreply.github.com>
2022-12-29 21:28:25 +0900

    named constructors

--- main.dart
+++ main.dart
@@ -1,8 +1,7 @@
 class Player {
   String name;
-  int age;
+  int age, xp;
   String team;
-  int xp;
 
   Player(
       {required this.name,
@@ -10,19 +9,19 @@ class Player {
       required this.team,
       required this.xp});
 
+  Player.createBluePlayer({required this.name, required this.age})
+      : team = "Blue Team",
+        xp = 0;
+
   void sayHello() {
-    print("Hello $name");
+    print("Hello $name from $team team with $xp xp and $age years old!");
   }
 }
 
 void main() {
-  Player player = Player(
-    name: "John",
-    age: 20,
-    team: "Team A",
-    xp: 100,
-  );
-  print(player.name);
-  print(player.age);
-  player.sayHello();
+  Player player1 = Player(name: "John", age: 20, team: "Red Team", xp: 100);
+  Player player2 = Player.createBluePlayer(name: "Jane", age: 21);
+
+  player1.sayHello();
+  player2.sayHello();
 }
